8d4ee22f2bfa86e7dcac51214ed0f8086b663809
iddimsangi <adammsangi@gmail.com>
2022-04-06 14:14:20 +0300

    improve: add open model functionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+//querySelector has limitation
+// const btn1 = document.querySelector('.show-modal');
+// btn1.addEventListener('click', () => {
+//   console.log('clicked button');
+// });
+const modelOpenHandler = () => {
+  console.log(`Button ${i} is clicked`);
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+};
+
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const showModalBtn = document.querySelectorAll('.show-modal');
+console.log(showModalBtn);
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', modelOpenHandler);
+}
