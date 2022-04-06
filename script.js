0f7db832202729ee95b66a2afe5151cd925f020a
iddimsangi <adammsangi@gmail.com>
2022-04-06 14:26:45 +0300

    improve: add close modal functionality

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 'use strict';
-//querySelector has limitation
-// const btn1 = document.querySelector('.show-modal');
-// btn1.addEventListener('click', () => {
-//   console.log('clicked button');
-// });
 const modelOpenHandler = () => {
-  console.log(`Button ${i} is clicked`);
   modal.classList.remove('hidden');
   overLay.classList.remove('hidden');
 };
 
+const modelCloseHandler = () => {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+};
+const closeButn = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overLay = document.querySelector('.overlay');
 const showModalBtn = document.querySelectorAll('.show-modal');
@@ -17,3 +16,4 @@ console.log(showModalBtn);
 for (let i = 0; i < showModalBtn.length; i++) {
   showModalBtn[i].addEventListener('click', modelOpenHandler);
 }
+closeButn.addEventListener('click', modelCloseHandler);
